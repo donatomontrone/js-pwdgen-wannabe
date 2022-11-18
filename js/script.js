889bba419601386525a32f6333d6fa65b0c63cab
donatomontrone <donato.montrone@outlook.com>
2022-11-18 16:45:10 +0100

    add head title and semicolons in js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 // Dichiarazione variabile nome
-const userName = prompt("Inserisci il tuo nome")
+const userName = prompt("Inserisci il tuo nome");
 
-document.getElementById('name').innerHTML= userName
+document.getElementById('name').innerHTML= userName;
 
 // Dichiarazione variabile cognome
-const userLastName = prompt("Inserisci il tuo cognonome")
+const userLastName = prompt("Inserisci il tuo cognonome");
 
-document.getElementById('lastname').innerHTML= userLastName
+document.getElementById('lastname').innerHTML= userLastName;
 
 //Dichiarazione variabile colore preferito
-const userFavoriteColor = prompt("Inserisci il tuo colore preferito")
+const userFavoriteColor = prompt("Inserisci il tuo colore preferito");
 
-document.getElementById('favorite-color').innerHTML= userFavoriteColor
+document.getElementById('favorite-color').innerHTML= userFavoriteColor;
 
 //Viasulizzazione delle variabili nella console
 
-console.log(userName, userLastName, userFavoriteColor)
+console.log(userName, userLastName, userFavoriteColor);
 
 //Dichiarazione variabile password
 
-const userPassword = userName + userLastName + userFavoriteColor + 22
+const userPassword = userName + userLastName + userFavoriteColor + 22;
 
-document.getElementById('password').innerHTML= userPassword
+document.getElementById('password').innerHTML= userPassword;
