fef551f091d61ef0ba28804c529b8bc778ccef3d
donatomontrone <donato.montrone@outlook.com>
2022-11-18 15:32:26 +0100

    create a password generator

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Dichiarazione variabile nome
+const userName = prompt("Inserisci il tuo nome")
+
+document.getElementById('name').innerHTML= userName
+
+// Dichiarazione variabile cognome
+const userLastName = prompt("Inserisci il tuo cognonome")
+
+document.getElementById('lastname').innerHTML= userLastName
+
+//Dichiarazione variabile colore preferito
+const userFavoriteColor = prompt("Inserisci il tuo colore preferito")
+
+document.getElementById('favorite-color').innerHTML= userFavoriteColor
+
+//Viasulizzazione delle variabili nella console
+
+console.log(userName, userLastName, userFavoriteColor)
+
+//Dichiarazione variabile password
+
+const userPassword = userName + userLastName + userFavoriteColor + 22
+
+document.getElementById('password').innerHTML= userPassword
